3143c5271b32ae7bd83401d3fba055440e25437b
schulzjo <schulzeeverding.johannes@gmail.com>
2020-08-26 20:45:39 +0200

    change from IndexedStack with Visibility  to traditional TabBarView

--- tabbar_container/lib/main.dart
+++ tabbar_container/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 final BoxDecoration bDec = BoxDecoration(
-      border: Border.all(
-        width: 1,
-        color: Colors.blue,
-      ),
-    );
+  border: Border.all(
+    width: 1,
+    color: Colors.blue,
+  ),
+);
 
 void main() {
   runApp(MyApp());
@@ -33,9 +33,9 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.grey[900]),
+      theme:
+          ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.grey[900]),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: ListView(
@@ -51,9 +51,11 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
               height: 50,
             ),
             Container(
-                margin: EdgeInsets.all(10),
-                decoration: bDec,
-                child: Column(children: <Widget>[
+              height: 300,
+              margin: EdgeInsets.all(10),
+              decoration: bDec,
+              child: Column(
+                children: <Widget>[
                   TabBar(
                     tabs: [
                       Container(height: 20, child: Text('Tab 1')),
@@ -61,58 +63,39 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                       Container(height: 20, child: Text('Tab 3'))
                     ],
                     controller: tabController,
-                    onTap: (int index) {
-                      setState(() {
-                        selectedIndex = index;
-                        tabController.animateTo(index);
-                      });
-                    },
+                    isScrollable: true,
                   ),
-                  Divider(height: 0),
-                  IndexedStack(
-                    children: <Widget>[
-                      Visibility(
-                        child: Container(
-                          height: 200,
-                          color: Colors.blue[100],
-                          child: Center(
-                            child: Text('Content 1',
-                                style: TextStyle(color: Colors.black)),
-                          ),
-                        ),
-                        maintainState: true,
-                        visible: selectedIndex == 0,
-                      ),
-                      Visibility(
-                        child: Container(
-                          height: 300,
+                  Expanded(
+                    child: TabBarView(
+                      controller: tabController,
+                      children: <Widget>[
+                        Container(
                           color: Colors.blue[200],
                           child: Center(
                             child: Text('Content 2',
                                 style: TextStyle(color: Colors.black)),
                           ),
                         ),
-                        maintainState: true,
-                        visible: selectedIndex == 1,
-                      ),
-                      Visibility(
-                        child: Container(
-                          height: 400,
+                        Container(
                           color: Colors.blue[300],
                           child: Center(
-                            child: Text(
-                              'Content 3',
-                              style: TextStyle(color: Colors.black),
-                            ),
+                            child: Text('Content 2',
+                                style: TextStyle(color: Colors.black)),
                           ),
                         ),
-                        maintainState: true,
-                        visible: selectedIndex == 2,
-                      ),
-                    ],
-                    index: selectedIndex,
+                        Container(
+                          color: Colors.blue[400],
+                          child: Center(
+                            child: Text('Content 3',
+                                style: TextStyle(color: Colors.black)),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ])),
+                ],
+              ),
+            ),
             Container(
               margin: EdgeInsets.all(10),
               decoration: bDec,
